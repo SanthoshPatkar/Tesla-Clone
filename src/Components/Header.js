7913c394f3ade4c5ae8af958c9e3ff2dbd9147b7
SanthoshPatkar <ssanthoshp159@gmail.com>
2021-12-24 16:57:25 +0530

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,8 +11,8 @@ function Header() {
                 <img src="/images/logo.svg" alt="" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
+                <a href="">Model S</a>
+                <a href="">Model 3</a>
                 <a href="#">Model Y</a>
                 <a href="#">Model X</a>
                 <a href="#">Solar Panels</a>
